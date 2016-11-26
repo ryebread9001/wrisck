3d3eb223aec92ef549fc2a016a14b9447b7972f6
jordan9001 <jordan9001@gmail.com>
2016-11-26 09:38:22 -0700

    Added the map client side

--- client.js
+++ client.js
@@ -7,3 +7,21 @@ var websocket_connection = new WebSocket(ws_host, "whitehead-protocol");
 websocket_connection.onmessage = function (evt) {
 	console.log(evt.data);
 };
+
+// Handle map interaction
+var hilight = document.getElementById('hilight');
+
+(function() {
+	var countries = document.getElementsByClassName('country');
+	for (var i=0; i<countries.length; i++) {
+		countries[i].addEventListener('mouseover', mouseoverCountry);
+	}
+})();
+
+function mouseoverCountry(evt) {
+	var country = evt.target;
+	var path = country.getAttribute('d');
+	hilight.setAttribute('d', path);
+	// TODO, draw connections
+	console.log(country.getAttribute('id'));
+}
